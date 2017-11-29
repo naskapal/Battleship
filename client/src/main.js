0afb97f94209f55d1cb4349e5ed8f32d39be3c1c
setiadijoe <setiadi_y56@yahoo.co.id>
2017-11-29 18:55:10 +0700

    storage database using firebase

--- client/src/main.js
+++ client/src/main.js
@@ -3,8 +3,17 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import firebase from 'firebase'
+
+let config = {
+  databaseURL: 'https://yonathan-e91f4.firebaseio.com',
+  projectId: 'yonathan-e91f4'
+}
+firebase.initializeApp(config)
 
 Vue.config.productionTip = false
+
+Vue.prototype.$db = firebase.database()
 
 /* eslint-disable no-new */
 new Vue({
