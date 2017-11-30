2c3a5be6014f7c88ddb30a6d79c40eeec0dabaa3
Septian Ahmad Fujianto <septianahmad@naisinpo.com>
2017-11-30 14:45:05 +0700

    Basic gameplay

--- client/src/components/SecondPlayer.vue
+++ client/src/components/SecondPlayer.vue
@@ -4,7 +4,7 @@
       <div class="row">
         <div class="col-md-6" :hidden="hidenPanel">
           <div class="row">
-            <div class="col-md-1 col-tile-1" v-for="data in datas"><button type="button" class="btn btn-primary" @click="countRound(data)" :disabled="panelBtn"></button></div>
+            <div class="col-md-1 col-tile-1" v-for="(data, index) in datas"><button type="button" class="btn btn-primary" @click="countRound(data, index, $event)" :disabled="panelBtn" :id="`player2-${index}`"></button></div>
           </div>
         </div>
         <div class="col-md-6">
@@ -15,17 +15,19 @@
     </div>
   </div>
 </template>
-
 <script>
   export default {
     name: 'SecondPlayer',
+    props: ['hit'],
     data () {
       return {
         atkBtn: false,
         hidenPanel: true,
         panelBtn: false,
+        isActive: false,
+        backgroundColor: 'red',
         counter: 0,
-        datas: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50]
+        datas: [[0,0], [0,1], [0,2], [0,3], [0,4], [0,5], [0,6], [0,7], [0,8], [0,9], [1,0], [1,1], [1,2], [1,3], [1,4], [1,5], [1,6], [1,7], [1,8], [1,9], [2,0], [2,1], [2,2], [2,3], [2,4], [2,5], [2,6], [2,7], [2,8], [2,9], [3,0], [3,1], [3,2], [3,3], [3,4], [3,5], [3,6], [3,7], [3,8], [3,9], [4,0], [4,1], [4,2], [4,3], [4,4], [4,5], [4,6], [4,7], [4,8], [4,9]]
       }
     },
     methods: {
@@ -33,18 +35,28 @@
         this.atkBtn = true
         this.hidenPanel = false
       },
-      countRound (data) {
+      countRound (data, index, event) {
+        this.$emit('attack-player-two', data);
+
+        console.log(data);
+
+        if (this.hit == true) {
+           document.querySelector(`#${event.currentTarget.id}`).style.backgroundColor = 'red'
+        } else {
+           document.querySelector(`#${event.currentTarget.id}`).style.backgroundColor = 'gray'
+        }
+
         this.counter++
-        if (this.counter >= 3) {
+
+        /*if (this.counter >= 3) {
           this.panelBtn = true
           this.atkBtn = false
           this.counter = 0
-        }
+        }*/
       }
     }
   }
 </script>
-
 <style>
 .secondPlayer button {
   width: 50px;
@@ -55,6 +67,6 @@
   height: 100vh;
   padding-top: 20vh;
   padding-bottom: 20vh;
-  background-color: #4fc7ef
+  background-color: #84b3ff
 }
 </style>
